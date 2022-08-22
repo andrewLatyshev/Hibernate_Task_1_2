5c9c8126df565ff757e1871cf1031d3e9289ffdc
Your Name <you@example.com>
2022-08-22 22:14:36 +0700

    2nd iterate, 2 tests passed: create and drop table

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,6 +1,7 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
 
@@ -8,29 +9,55 @@ public class Main {
     public static void main(String[] args) {
 //        Util util = new Util();
 //        util.getConnection();
+        UserServiceImpl user = new UserServiceImpl();
+        user.createUsersTable();
 
         Session session = Util.getSessionFactory().openSession();
         session.beginTransaction();
 
-        User user = new User();
-        user.setName("Andrew");
-        user.setLastName("Latyshev");
-        user.setAge((byte) 35);
+        user.createUsersTable();
+        session.save(new User("Andrew", "Latyshev", (byte) 35));
+        session.save(new User("Svetlana", "Latysheva", (byte) 29));
+        session.save(new User("Leonid", "Podsohin", (byte) 32));
+        session.save(new User("Mikhail", "Karasev", (byte) 34));
+//        session.remove(user);
 
-        user.setName("Svetlana");
-        user.setLastName("Latysheva");
-        user.setAge((byte) 29);
 
-        user.setName("Leonid");
-        user.setLastName("Podsohin");
-        user.setAge((byte) 32);
+//        user.saveUser("Andrew", "Latyshev", (byte) 35);
+//        user.saveUser("Svetlana", "Latysheva", (byte) 29);
+//        user.saveUser("Leonid", "Podsohin", (byte) 32);
+//        user.saveUser("Mikhail", "Karasev", (byte) 34);
 
-        user.setName("Mikhail");
-        user.setLastName("Karasev");
-        user.setAge((byte) 35);
+//        user.removeUserById(1);
+//        user.getAllUsers();
+//        user.cleanUsersTable();
+//        user.dropUsersTable();
 
-        session.getTransaction().commit();
-        Util.shutdown();
+        System.out.println(user);
+
+        session.close();
+
+
+
+//        User user = new User();
+//        user.setName("Andrew");
+//        user.setLastName("Latyshev");
+//        user.setAge((byte) 35);
+//
+//        user.setName("Svetlana");
+//        user.setLastName("Latysheva");
+//        user.setAge((byte) 29);
+//
+//        user.setName("Leonid");
+//        user.setLastName("Podsohin");
+//        user.setAge((byte) 32);
+//
+//        user.setName("Mikhail");
+//        user.setLastName("Karasev");
+//        user.setAge((byte) 35);
+//
+//        session.getTransaction().commit();
+//        Util.shutdown();
 //
 //        user.createUsersTable();
 //        user.saveUser("Andrew", "Latyshev", (byte) 35);
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,14 +1,15 @@
 package jm.task.core.jdbc.dao;
 
-import jakarta.persistence.Query;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
+
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
@@ -24,7 +25,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void createUsersTable() {
         try {
-            PreparedStatement preparedStatement = new Util().getConnection().prepareStatement("create table users (id INT auto_increment primary key not null , name NVARCHAR(20) not null , lastName NVARCHAR(20) not null, age INT not null)");
+            PreparedStatement preparedStatement = new Util().getConnection().prepareStatement("create table user (id INT auto_increment primary key not null , name NVARCHAR(20) not null , lastName NVARCHAR(20) not null, age INT not null)");
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -33,7 +34,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void dropUsersTable() {
         try {
-            PreparedStatement preparedStatement = new Util().getConnection().prepareStatement("DROP TABLE users");
+            PreparedStatement preparedStatement = new Util().getConnection().prepareStatement("DROP TABLE user");
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -42,17 +43,33 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        sessionFactory.openSession();
-        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+
+//        User user = new User("Andrew", "Latyshev", (byte) 35);
+        session.save(User.class);
+        session.getTransaction().commit();
+
+        sessionFactory.close();
 
     }
 
     @Override
     public void removeUserById(long id) {
+        session.beginTransaction();
+
+        User user = session.get(User.class, id);
+        session.remove(user);
+        session.getTransaction().commit();
+
+        sessionFactory.close();
+
     }
 
     @Override
     public List<User> getAllUsers() {
+
+        session.beginTransaction();
+
 
         CriteriaBuilder cb = sessionFactory.getCriteriaBuilder();
         CriteriaQuery cq = cb.createQuery(User.class);
@@ -60,9 +77,11 @@ public class UserDaoHibernateImpl implements UserDao {
         cq.select(user);
 
         Query query = session.createQuery(cq);
-        List<User> users = query.getResultList();
 
-        session.close();
+        List<User> users = query.getResultList();
+        session.getTransaction().commit();
+
+        sessionFactory.close();
 
         return users;
     }
